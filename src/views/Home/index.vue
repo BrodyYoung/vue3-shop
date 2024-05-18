d82a37ca3abd41244c5bce764df999be43fa7211
Brody Young <yungyunbo@gmail.com>
2024-05-19 07:37:28 +0800

    feat:首页布局

---
+++ src/views/Home/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+
+</script>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <homeProduct />
+
+  <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+    <HomeNew />
+  </HomePanel>
+
+  <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
+    <HomeHot />
+  </HomePanel>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup xmlns="">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+<!--    <TheWelcome />-->
+    <el-button type="primary"></el-button>
+    <el-button type="success"></el-button>
+    <el-button type="danger"></el-button>
+    <el-button type="error"></el-button>
+    <el-button type="warning"></el-button>
+  </main>
+</template>
